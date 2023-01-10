857e1947fbb56e4355d1a2b9687b8534f500dd59
Jonghyun-Joo <jojo@monymony.co>
2023-01-10 09:22:19 +0900

    feat : hello graphql

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { buildSchema } from "graphql";
+import { graphqlHTTP } from "express-graphql";
 
 dotenv.config();
+
+const schema = buildSchema(`
+type Query{
+  hello: String
+}`);
+
+const root = { hello: () => "Hello world!" };
 
 const app: Express = express();
 const port = process.env.PORT;
@@ -9,7 +18,16 @@ const port = process.env.PORT;
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`Now browse to localhost:${port}/graphql`);
 });
